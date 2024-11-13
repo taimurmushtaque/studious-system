1cf2535c6e1d3a84485bc5853432b2218afd62ed
Taimur Mushtaq <taimurmushtaq@outlook.com>
2024-11-14 03:07:37 +0500

    Added counter app files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+increment = () => {
+    count += 1
+    countEl.textContent = count
+}
+
+decrement = () => {
+    count -= 1
+    countEl.textContent = count
+}
+
+function save() {
+    if (count < 0) {
+        return
+    }
+    else {
+        let countStr = count + " - "
+        saveEl.textContent += countStr
+        countEl.textContent = 0
+        count = 0
+    }
+}
